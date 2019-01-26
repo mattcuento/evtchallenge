0270a9b550600aef8df389400c69c9c2d9790a49
Matt Cuento <cuentomr17@gmail.com>
2019-01-25 16:41:40 -0800

    Changes

--- project.py
+++ project.py
@@ -8,27 +8,10 @@ file = "index.html"
 #translating url into text
 webpage = urllib.request.urlopen(url)
 html = webpage.read()
-print(html)
 
 #writing text to index.html file
 with open(file, "wb") as text_file:
 	text_file.write(html)
 
 #auto open default browser to index.html
-<<<<<<< HEAD
-webbrowser.open_new_tab("file://" + os.path.realpath(file))
-
-#specifying local port number
-PORT = 8000
-
-#create the request handler
-reqHandler = http.server.SimpleHTTPRequestHandler
-
-#create the TCP server and pass in port and reqHandler
-#returns the active port and begins running server, active on any IP
-with socketserver.TCPServer(("", PORT), reqHandler) as httpd:
-	print("Active at port", PORT)
-	httpd.serve_forever()
-=======
 webbrowser.open_new_tab("192.168.99.100:80")
->>>>>>> 9cd44d234d303c8780591830ae2f6640082eec5c

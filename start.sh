501f538e0c984f5ea30a16ef64bf5f1f8fb11f74
Matt <cuentomr17@gmail.com>
2019-01-26 19:57:40 -0800

    Done!

--- start.sh
+++ start.sh
@@ -3,9 +3,4 @@
 sudo apt-get update
 sudo apt-get install python3.6
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
 python3 project.py
-
-python -m webbrowser -t "http://www.google.com"

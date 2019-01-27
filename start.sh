e71f4e40c8ac3b7afc0e5714528b0c554ad646a7
Matt Cuento <cuentomr17@gmail.com>
2019-01-26 18:36:16 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,5 @@
 sudo apt-get update
 sudo apt-get install python3.6
 
-xtract the tarball
-tar xvfz Python-3.5.1.tgz
-Configure and Install:
-cd Python-3.5.1
-./configure --prefix = /opt/python3.5.1
-make  
-sudo make install
 
-python project.py
+python3 project.py
